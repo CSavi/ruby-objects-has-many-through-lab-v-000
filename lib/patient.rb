f8397fa29d7aa7f91b43fb4d9ef061aa62f77739
Carrie Savinell <carrielane89@gmail.com>
2018-05-29 19:34:06 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -18,5 +18,6 @@ class Patient
   end 
   
   def appointments 
-    Appointment.all.collect {|appointment| appointment.patient} 
+    Appointment.all.collect {|appointment| appointment.patient}
+  end   
 end   
